a45c2bd18d0ceda3897e4115caf59e8954d30a5a
Emreacikay <hasanemre.acikay@hotmail.com>
2022-10-21 17:33:47 +0300

    21.10.2022

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using ETicaretAPI.Application.Abstractions;
-using ETicaretAPI.Persistance.Concretes;
+using ETicaretAPI.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,7 @@ namespace ETicaretAPI.Persistance
     {
         public static void AddPersistanceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ETicaretAPIDb;"));
         }
     }
 }
